5b5d9924810dfdc344e146dbd4a5a2cc7e7a14df
granawkins <granthawkins88@gmail.com>
2022-06-20 10:24:54 +0700

    fix regression test
    The record shouldve been updated in a previous commit when sigfig round was
    implemented, but the MacOS exception made it so I didnt notice the error
    locally. That MacOS exception is no longer needed and has also been
    commented out.

--- karoo_gp/test/test_cli.py
+++ karoo_gp/test/test_cli.py
@@ -46,12 +46,13 @@ def test_cli(tmp_path, paths, ker, typ, bas, seed):
             actual_dataset.relative_to(paths.root) ==
             expected_dataset)
 
-    if ker == 'r' and typ == 'f' and platform.mac_ver()[-1] == 'arm64':
-        # the Apple M1 seems to have some accuracy problem that leads
-        # this test to fail, so extract the score and approximate it
-        actual_score = actual_json.pop('score')
-        expected_score = expected_json.pop('score')
-        assert pytest.approx(actual_score) == expected_score
+    # Not needed after implementing sigfig_round in the Regression kernel
+    # if ker == 'r' and typ == 'f' and platform.mac_ver()[-1] == 'arm64':
+    #     # the Apple M1 seems to have some accuracy problem that leads
+    #     # this test to fail, so extract the score and approximate it
+    #     actual_score = actual_json.pop('score')
+    #     expected_score = expected_json.pop('score')
+    #     assert pytest.approx(actual_score) == expected_score
 
     # compare the content of the two jsons
     assert actual_json == expected_json
